88633c1cd7e267679fbadd337c754b3c39c2fd9d
Subhadeep25 <84439620+Subhadeeep25@users.noreply.github.com>
2024-01-16 19:33:24 +0530

    Time: 115 ms (32.75%) | Memory: 22.4 MB (6.91%) - LeetSync

---
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool samecharacters(unordered_map<char,int>mp1,string word2){
+        for(auto i:word2){
+            if(mp1.find(i)==mp1.end())return false;
+        }
+        return true;
+    }
+    bool closeStrings(string word1, string word2) {
+        unordered_map<char,int>mp1,mp2;
+        if(word1.size()!=word2.size())return false;
+        for(auto i:word1){
+            mp1[i]++;
+        }
+        for(auto i:word2){
+            mp2[i]++;
+        }
+        for(auto i = mp1.begin(); i != mp1.end(); i++){
+            cout<<i->first<<i->second<<";";
+        }
+        for(auto i = mp1.begin(); i != mp1.end(); i++){
+            bool flag=false;
+            for(auto j = mp2.begin(); j != mp2.end(); j++){
+                if(i->second==j->second){
+                    cout<<i->first<<i->second<<" "<<j->first<<j->second<<endl;
+                    mp2[j->first]=0;
+                    flag=true;
+                    break;
+                }
+            }
+            if(flag==false){
+                cout<<1;
+                return false;
+                
+            }
+        }
+        return samecharacters(mp1,word2) && true;
+    }
+};
